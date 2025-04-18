353b44fa1e333e4e8edf68bbc3c5da60a1dff8f2
zhi.zhuang <>
2025-04-18 19:27:20 +0800

    修改成mvc

--- api/route/route.go
+++ api/route/route.go
@@ -3,23 +3,59 @@ package route
 import (
 	"time"
 
+	"github.com/amitshekhariitbhu/go-backend-clean-architecture/api/controller"
 	"github.com/amitshekhariitbhu/go-backend-clean-architecture/api/middleware"
 	"github.com/amitshekhariitbhu/go-backend-clean-architecture/bootstrap"
+	"github.com/amitshekhariitbhu/go-backend-clean-architecture/domain"
 	"github.com/amitshekhariitbhu/go-backend-clean-architecture/mongo"
+	"github.com/amitshekhariitbhu/go-backend-clean-architecture/repository"
+	"github.com/amitshekhariitbhu/go-backend-clean-architecture/usecase"
 	"github.com/gin-gonic/gin"
 )
 
 func Setup(env *bootstrap.Env, timeout time.Duration, db mongo.Database, gin *gin.Engine) {
 	publicRouter := gin.Group("")
+
+	ur := repository.NewUserRepository(db, domain.CollectionUser, domain.CollectionUserMapping, domain.CollectionAccount)
+	sc := controller.SignupController{
+		SignupUsecase: usecase.NewSignupUsecase(ur, timeout),
+		Env:           env,
+	}
+	publicRouter.POST("/signup", sc.Signup)
+
+	lc := &controller.LoginController{
+		LoginUsecase: usecase.NewLoginUsecase(ur, timeout),
+		Env:          env,
+	}
+	publicRouter.POST("/login", lc.Login)
+
+	rtc := &controller.RefreshTokenController{
+		RefreshTokenUsecase: usecase.NewRefreshTokenUsecase(ur, timeout),
+		Env:                 env,
+	}
+	publicRouter.POST("/refresh", rtc.RefreshToken)
+
 	// All Public APIs
-	NewSignupRouter(env, timeout, db, publicRouter)
-	NewLoginRouter(env, timeout, db, publicRouter)
-	NewRefreshTokenRouter(env, timeout, db, publicRouter)
+	// NewSignupRouter(env, timeout, db, publicRouter)
+	// NewLoginRouter(env, timeout, db, publicRouter)
+	// NewRefreshTokenRouter(env, timeout, db, publicRouter)
 
 	protectedRouter := gin.Group("")
 	// Middleware to verify AccessToken
 	protectedRouter.Use(middleware.JwtAuthMiddleware(env.AccessTokenSecret))
+
+	pc := &controller.ProfileController{
+		ProfileUsecase: usecase.NewProfileUsecase(ur, timeout),
+	}
+	protectedRouter.GET("/profile", pc.Fetch)
+
+	tr := repository.NewTaskRepository(db, domain.CollectionTask)
+	tc := &controller.TaskController{
+		TaskUsecase: usecase.NewTaskUsecase(tr, timeout),
+	}
+	protectedRouter.GET("/task", tc.Fetch)
+	protectedRouter.POST("/task", tc.Create)
 	// All Private APIs
-	NewProfileRouter(env, timeout, db, protectedRouter)
-	NewTaskRouter(env, timeout, db, protectedRouter)
+	// NewProfileRouter(env, timeout, db, protectedRouter)
+	// NewTaskRouter(env, timeout, db, protectedRouter)
 }
